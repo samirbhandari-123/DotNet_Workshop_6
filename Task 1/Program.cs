18fc23bb126ce3915be6d6fc6f13efb20ec4eadf
samirbhandari-123 <samirbhandari690@gmail.com>
2025-12-03 11:24:05 +0545

    Task 1 completed

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Task_1
+﻿using System;
+
+namespace Task_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a Rectangle object
+            Rectangle rect = new Rectangle();
+
+            // Get user input
+            Console.Write("Enter length: ");
+            rect.Length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter breadth: ");
+            rect.Breadth = Convert.ToDouble(Console.ReadLine());
+
+            // Display rectangle details
+            Console.WriteLine(rect.ShowDetails());
+
+            // Keep console window open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
---
+++ Task 1/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        // Expression-bodied properties
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+        // Expression-bodied method to calculate area
+        public double Area() => length * breadth;
+
+        // Expression-bodied method to calculate perimeter
+        public double Perimeter() => 2 * (length + breadth);
+
+        // Expression-bodied method to display rectangle details
+        public string ShowDetails() =>
+            $"Rectangle Details:\nLength: {length}\nBreadth: {breadth}\nArea: {Area()}\nPerimeter: {Perimeter()}";
+    }
+}
